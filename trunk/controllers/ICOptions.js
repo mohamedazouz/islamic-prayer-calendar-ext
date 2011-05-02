12a7226c3f3f7738a26bce6d432a6cc42bb4db71
gemy21ce@gmail.com <gemy21ce@gmail.com@8df4df63-37b6-df96-8f3c-80e2325f9bc8>
2011-05-02 22:20:17 +0000

    root

--- trunk/controllers/ICOptions.js
+++ trunk/controllers/ICOptions.js
@@ -6,7 +6,160 @@ icOptions = function(){
     var icOptions={};
     $(function(){
 
-    });
+        });
     return icOptions;
 }
 var icOptions= new icOptions();
+
+var background=chrome.extension.getBackgroundPage();
+
+var lat,lng,gmt,country,countryCode,timezoneId,city,formatted_address;
+var map,marker,geocoder,center;
+var Positioning={
+    initialize:function(){
+        var myOptions = {
+            zoom: 6,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            streetViewControl:false
+        };
+        // initializing the map and the geocoder
+        geocoder = new google.maps.Geocoder();
+        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+        // setting the map center to the current position
+        moveToCurrent();
+
+        //adding lister to changer lat,lng as the zoom changed.
+        google.maps.event.addListener(map, 'zoom_changed', function() {
+            center = map.getCenter();
+            marker.setPosition(center);
+            moveToDarwin(center.lat(),center.lng());
+            Positioning.newPosition();
+        });
+
+        //adding listener to dragging the map
+        google.maps.event.addListener(map, "dragend", function() {
+            Positioning.newPosition();
+        });
+
+        google.maps.event.addListener(map, "drag", function() {
+            center = map.getCenter();
+            marker.setPosition(center);
+            moveToDarwin(center.lat(), center.lng());
+        });
+
+        function moveToDarwin(lat,lng){
+            if(! lat || ! lng){
+                moveToCurrent();
+                return;
+            }
+            center = new google.maps.LatLng(lat,lng);
+            map.setCenter(center);
+        }
+
+        function moveToCurrent(){
+            // Try W3C Geolocation (Preferred)
+            if(navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    center = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+                    marker = new google.maps.Marker({
+                        map: map,
+                        position: center
+                    });
+                    marker.setDraggable(true);
+                    google.maps.event.addListener(marker,'dragend',function(){
+                        center=marker.getPosition();
+                        map.setCenter(center);
+                        Positioning.newPosition();
+                    });
+                    map.setCenter(center);
+                }, function() {
+                    handleNoGeolocation();
+                });
+            }
+        }
+
+        function handleNoGeolocation() {
+            map.setCenter(new google.maps.LatLng(43.834526782236814, -37.265625));
+            map.setOptions({
+                zoom:2
+            });
+        }
+    },
+    codeAddress:function(){
+        var address = document.getElementById("address").value;
+        if(address == ''){
+            return;
+        }
+        geocoder.geocode( {
+            'address': address
+        }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+                marker.setPosition(results[0].geometry.location);
+                marker.setDraggable(true);
+                google.maps.event.addListener(marker,'dragend',function(){
+                    center=marker.getPosition();
+                    map.setCenter(center);
+                    Positioning.newPosition();
+                });
+                map.setOptions({
+                    zoom:6
+                });
+                Positioning.newPosition();
+            } else {
+                console.log("Geocode was not successful for the following reason: " + status);
+            }
+        });
+    },
+    newPosition:function(){
+        var latDif,lngDif;
+        if(lat && lng){
+            latDif = Math.sqrt((center.lat()) * (center.lat())) - lat;
+            latDif = Math.sqrt(latDif * latDif);
+
+            lngDif = Math.sqrt((center.lng()) * (center.lng())) - lng;
+            lngDif = Math.sqrt(lngDif * lngDif);
+
+            if(latDif > 0.5 || lngDif > 0.5){
+                Positioning.geonamesVars();
+            }
+        }else{
+            Positioning.geonamesVars();
+        }
+        lat = Math.sqrt( (center.lat()) * (center.lat()));
+        lng = Math.sqrt( (center.lng()) * (center.lng()));
+    },
+    geonamesVars:function(){
+        background.Positioning.geonamesVars(center.lat(),center.lng(),function(ob){
+            gmt=ob.gmtOffset;
+            timezoneId=ob.timezoneId;
+        });
+        background.Positioning.googlemapsLocation(center.lat(),center.lng(),function(ob){
+            for(var i = 0 ; i < ob.results.length; i++){
+                if($.inArray("administrative_area_level_2", ob.results[i].types) != -1/* ||  $.inArray("street_address", ob.results[i].types) != -1*/){
+                    formatted_address=ob.results[i].formatted_address;
+                    for(j in ob.results[i].address_components){
+                        if($.inArray("country", ob.results[i].address_components[j].types) != -1){
+                            country = ob.results[i].address_components[j].long_name;
+                            countryCode = ob.results[i].address_components[j].short_name;
+                        }
+                        if($.inArray("administrative_area_level_1", ob.results[i].address_components[j].types) != -1){
+                            city = ob.results[i].address_components[j].long_name;
+                        }
+                    }
+                    i = ob.results.length;
+                }
+            }
+            $("#country").html(formatted_address);
+        });
+    }
+}
+
+$(function(){
+    Positioning.initialize();
+    $("#gotoButton").click(function(){
+        Positioning.codeAddress();
+    });
+    window.setTimeout(Positioning.newPosition, 1000);
+})
---
+++ trunk/controllers/util.js
@@ -0,0 +1,245 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var util={
+    /**
+     * clone object.
+     * @return the new cloned object
+     * @param object the object to be cloned.
+     */
+    clone:function (object) { // shallow clone
+        var clone = {};
+        for (var key in object) {
+            clone[key] = object[key];
+        }
+        return clone;
+    },
+    /**
+     * cut text with respect to word completion.
+     * @param text the text to be cutted.
+     * @param length aproximatly length to cut text after, not exactly the length of the new text.
+     * @param suffix the suffix text after cutted text.
+     * @return the new cutted text
+     */
+    cutText:function(text,length,suffix){
+        if(! suffix){
+            suffix = '';
+        }
+        for( i =0 ; i< 20 ; i++){
+            if(text.charAt( length) != " "){
+                length++;
+            }else{
+                i=20;
+            }
+        }
+        return text.substring(0, length) + " " + suffix;
+    },
+    /**
+     * get alist of checked elements value in the page.
+     * @return an array of checked elements values.
+     */
+    selectedRows:function(){
+        var checked=[];
+        $(':checkbox').each(function(){
+            if(this.checked){
+                checked.push(this.value);
+            }
+        });
+        return checked;
+    },
+    /**
+     * returns the index of the object in the array, the object must be similar to the array contains. 
+     * @param ob the object to get its index.
+     * @param array the array to check throw.
+     * @param compareItem the item that will compare with in the array items, if null the object will be compared as string.
+     * @return the index of the element, -1 if it's not exists.
+     */
+    inObjectArray:function(ob,array,compareItem){
+        var index=-1;
+        for(i=0; i< array.length; i++){
+            if(compareItem){
+                if(ob[compareItem] == array[i][compareItem]){
+                    index = i;
+                    i=array.length;
+                }
+            }else{
+                if(JSON.stringify(ob) == JSON.stringify(array[i])){
+                    index = i;
+                    i=array.length;
+                }
+            }
+        }
+        return index;
+    },
+    /**
+     * revers a string.
+     * @param str the string to be reversed.
+     * @return the reversed string.
+     * @example var x=reverseString('abcd');
+     *          console.log(x);//this prints dcba
+     *          
+     */
+    reverseString:function(str){
+        var i=str.length;
+        i=i-1;
+        var revstr=""
+        for (var x = i; x >=0; x--)
+        {
+            revstr+=str.charAt(x);
+        }
+        return revstr;
+    },
+    /**
+     * replaces all match strings in the given string.
+     * @param str original string.
+     * @param match the match string.
+     * @param replace the string to replace with.
+     * @return the new string.
+     * @deprecated original 1.2 js lib str.replace(/regix/g,replac) see the example.
+     * @example replaceAll('abcdefghiabc', "ab", "x");
+     *          //you can use javascript replace with regix
+     *          var str='abcdefghiabc';
+     *          var replace=str.replace(/abc/g,'x');
+     *          console.log(replace);//prints xdefghix
+     *
+     */
+    replaceAll:function(str,match,replace){
+        var retstr=str;
+        while(retstr.indexOf(match) != -1){
+            retstr=retstr.replace(match, replace);
+        }
+        return retstr;
+    },
+    /**
+     * From JQuery 1.4.4
+     * Copyright 2010, John Resig
+     * Dual licensed under the MIT or GPL Version 2 licenses.
+     * http://jquery.org/license
+     */
+    extend:function(){
+        var options, name, src, copy, copyIsArray, clone,
+        target = arguments[0] || {},
+        i = 1,
+        length = arguments.length,
+        deep = false;
+
+        // Handle a deep copy situation
+        if ( typeof target === "boolean" ) {
+            deep = target;
+            target = arguments[1] || {};
+            // skip the boolean and the target
+            i = 2;
+        }
+
+        // Handle case when target is a string or something (possible in deep copy)
+        if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
+            target = {};
+        }
+
+        // extend jQuery itself if only one argument is passed
+        if ( length === i ) {
+            target = this;
+            --i;
+        }
+
+        for ( ; i < length; i++ ) {
+            // Only deal with non-null/undefined values
+            if ( (options = arguments[ i ]) != null ) {
+                // Extend the base object
+                for ( name in options ) {
+                    src = target[ name ];
+                    copy = options[ name ];
+
+                    // Prevent never-ending loop
+                    if ( target === copy ) {
+                        continue;
+                    }
+
+                    // Recurse if we're merging plain objects or arrays
+                    if ( deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) ) ) {
+                        if ( copyIsArray ) {
+                            copyIsArray = false;
+                            clone = src && jQuery.isArray(src) ? src : [];
+
+                        } else {
+                            clone = src && jQuery.isPlainObject(src) ? src : {};
+                        }
+
+                        // Never move original objects, clone them
+                        target[ name ] = jQuery.extend( deep, clone, copy );
+
+                        // Don't bring in undefined values
+                    } else if ( copy !== undefined ) {
+                        target[ name ] = copy;
+                    }
+                }
+            }
+        }
+
+        // Return the modified object
+        return target;
+    },
+    /**
+     * get a url form string sequence.
+     * @param str to seek the url throw.
+     * @return the url in the string.
+     */
+    getURL:function(str){
+        function URLLength(string,urlPos){
+            var Length=0;
+            for(i=urlPos;i<string.length;i++){
+                Length++;
+                if(string.charAt(i)==' '|| string.charAt(i) == "\n"){
+                    i=string.length;
+                }
+            }
+            return Length;
+        }
+        function getHttp(str){
+            httpurl=str.indexOf("http://", 0);
+            httpsurl=str.indexOf("https://", 0);
+            if(httpurl ==-1 && httpsurl== -1){
+                return null;
+            }
+            var strurl;
+            if(httpsurl != -1){
+                strurl=str.substr(httpsurl, URLLength(str,httpsurl));
+            }else{
+                strurl=str.substr(httpurl, URLLength(str,httpurl));
+            }
+            return strurl;
+            
+        }
+        function getWWW(str){
+            wwwurl=str.indexOf('www.', 0);
+            if (wwwurl == -1){
+                return null;
+            }
+            return str.substr(wwwurl,URLLength(str, wwwurl));
+        }
+        var url= getHttp(str);
+        return url==null? getWWW(str):url;
+    },
+    parseHTML:function(html){
+        var el ={};
+        if(html.indexOf("<img") != -1){
+            var img=html.substring(html.indexOf("src=\"",html.indexOf("<img"))+5,html.indexOf("\"",html.indexOf("src=\"",html.indexOf("<img"))+11));
+            el.imgsrc=img;
+        }else{
+            el.imgsrc=null;
+        }
+
+
+//        html=html.substring(html.lastIndexOf("</a>"), html.indexOf("<a"));
+
+        html=html.replace(/&(lt|gt);/g, function (strMatch, p1){
+            return (p1 == "lt")? "<" : ">";
+        });
+        var description=html.replace(/<\/?[^>]+(>|$)/g, " ");
+
+        el.description=description;
+
+        return el;
+    }
+}
